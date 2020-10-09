297e9e4853b271f55d81a2c6b3639df2543d1835
Trey <tajohns22@gmail.com>
2020-10-09 16:50:21 -0500

    solved find-rotated-index and find-rotation-count problems

--- find-rotated-index.js
+++ find-rotated-index.js
@@ -1,5 +1,62 @@
-function findRotatedIndex() {
- 
+/*find the index of a rotated arr
+//findRotatedIndex([3,4,1,2],4) // 1
+findRotatedIndex([6, 7, 8, 9, 1, 2, 3, 4], 8) // 2
+findRotatedIndex([6, 7, 8, 9, 1, 2, 3, 4], 3) // 6
+findRotatedIndex([37,44,66,102,10,22],14) // -1
+findRotatedIndex([6, 7, 8, 9, 10, 11, 12 1, 2, 3, 4], 4) // -1
+[7,8,1,2], 2
+*/
+function findRotatedIndex(arr, target) {
+    let leftIdx = 0;
+    let rightIdx = arr.length - 1;
+
+    if(arr[leftIdx] > arr[rightIdx]){
+        middleIdx = Math.floor((leftIdx + rightIdx)/2);
+        let found =  findRotatedIndex(arr.slice(leftIdx, middleIdx + 1), target);
+        
+        if(found === -1){
+            found = findRotatedIndex(arr.slice(middleIdx + 1, rightIdx + 1), target)
+            if (found === -1) return found
+            else {
+
+                return found + arr.slice(leftIdx, middleIdx + 1).length
+            }
+        } 
+            return found
+        
+    }
+
+   
+   
+    return binarySearch(arr, target)
+
+    //divide the array once into an upper and lower bound
+   
+    //search through the upper index bound using binary search
+    //return the index if found else return -1
+    // if found end the code else continue
+    //search the lower index bound and repeat repeat steps above
+    //end code
+
+}
+
+function binarySearch(arr,target){
+    let leftIdx = 0;
+    let rightIdx = arr.length - 1;
+
+    while(leftIdx <= rightIdx){
+        let middleIdx = Math.floor((leftIdx + rightIdx)/2)
+        let middleVal = arr[middleIdx]
+
+        if(middleVal < target){
+            leftIdx = middleIdx + 1;
+        } else if (middleVal > target){
+            rightIdx = middleIdx - 1;
+        } else {
+            return middleIdx
+        }
+    }
+    return -1;
 }
 
 module.exports = findRotatedIndex
--- find-rotation-count.js
+++ find-rotation-count.js
@@ -1,5 +1,40 @@
-function findRotationCount() {
-  
+/**
+ * 
+ * find the amount of times a sorted array has been rotated
+ * notes:
+ *  what ever the smallest number index is at in the sorted array is the amount of times its been rotated
+ * so find the smallest number in a rotated index
+ * split the array
+ * check if the middle is greater than the right side
+ * if it is the array is still rotated
+ * if it is not the array is not rotated and middle would be the smallest number
+ * ex [6,7,8,1,2,4]
+ *     [8,1,2,4]
+ *      [1,2,4]
+ *      
+ *  
+ */
+function findRotationCount(arr) {
+let leftIdx = 0;
+let rightIdx = arr.length - 1;
+
+//if the left is greater than the right the array is rotated
+while (arr[leftIdx] > arr[rightIdx]){
+    let midIdx = Math.floor((leftIdx+rightIdx)/2);
+    let midVal = arr[midIdx]
+
+    //if the middle is greater than the right the it is still rotated
+    if(midVal > arr[rightIdx]){
+        //move left to the mid
+        leftIdx = midIdx + 1;
+    } else {
+        //if the mid is less than or equal to the right than I know the mid is the smallest value 
+        //return its index to get the amount of times the array has been rotated.
+        rightIdx = midIdx;
+    }
+}
+
+return leftIdx;
 }
 
 module.exports = findRotationCount

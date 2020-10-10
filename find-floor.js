1a1bad85eca9b7931625461e40ec0f3ba46b8e35
Trey <tajohns22@gmail.com>
2020-10-10 12:13:09 -0500

    completed find-floor problem

--- find-floor.js
+++ find-floor.js
@@ -1,5 +1,22 @@
-function findFloor() {
-  
+function findFloor(arr, target) {
+  let leftIdx = 0
+  let rightIdx = arr.length - 1
+  let tempFloor = -1;
+
+  while(leftIdx <= rightIdx){
+      let middleIdx = Math.floor((leftIdx + rightIdx)/2);
+      let middleVal = arr[middleIdx]
+
+      if(middleVal > target){
+          rightIdx = middleIdx - 1;
+      } else if (middleVal < target){
+          leftIdx = middleIdx + 1;
+          tempFloor = Math.max(arr[middleIdx], tempFloor)
+      } else {
+          return tempFloor = arr[middleIdx]
+      }
+  }
+  return tempFloor
 }
 
 module.exports = findFloor
